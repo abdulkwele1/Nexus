176683c1373b7d728ab251546aeba36559c5383e
abdulkwele1 <168374411+abdulkwele1@users.noreply.github.com>
2024-09-24 18:24:59 -0700

    Removing the navbar on solar page again
    (cherry picked from commit d009876cc0c9a3d09aea65f14171287f91af594d)

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -2,24 +2,34 @@
 import { computed } from 'vue';
 import { useRoute } from 'vue-router';
 
-// Get the current route
 const route = useRoute();
 
-// Stops navbar from appearing on certain pages
 const isHomePage = computed(() => route.path === '/home');
 const isSolarPage = computed(() => route.path === '/solar');
-const isLocations = computed(() => route.path ==='/locations');
+const isLocations = computed(() => route.path === '/locations');
 </script>
 
 <template>
-  <nav v-if="!isSolarPage, !isLocations"  style="background-color: #FFFFFF; border-color: #e5e7eb;">
+  <nav v-if="!isSolarPage && !isLocations" style="background-color: #FFFFFF; border-color: #e5e7eb;">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
         <img alt="Vue logo" class="logo" src="@/assets/nexus.png" width="175" height="150" />
       </a>
-      <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+      <button
+        data-collapse-toggle="navbar-default"
+        type="button"
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        aria-controls="navbar-default"
+        aria-expanded="false"
+      >
         <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+        <svg
+          class="w-5 h-5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 17 14"
+        >
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
       </button>
