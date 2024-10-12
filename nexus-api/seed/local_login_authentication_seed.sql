466382b6bcc24f92cb447ff285819f8d19dd1a3b
Levi Schoen <l.schoen@kontakt.io>
2024-10-12 11:50:16 -0700

    add demo user to user seed file

--- nexus-api/seed/local_login_authentication_seed.sql
+++ nexus-api/seed/local_login_authentication_seed.sql
@@ -2,5 +2,6 @@ INSERT INTO "login_authentications"
  ("id", "user_name", "password_hash") 
  VALUES 
  (DEFAULT, 'levi', '$2a$10$HqQx4jxUzfQm1fZYUZRLbOBaMNWHmhSmweH03rl0EykgE4BNfDciO'),
- (DEFAULT, 'abdul', '$2a$14$KXCe7VMOjZdf/BwSKIFLxu2FRHcr.DAQntjq8OfdqQI69EOQz4gHW')
+ (DEFAULT, 'abdul', '$2a$14$KXCe7VMOjZdf/BwSKIFLxu2FRHcr.DAQntjq8OfdqQI69EOQz4gHW'),
+ (DEFAULT, 'demo', '$2a$10$HqQx4jxUzfQm1fZYUZRLbOBaMNWHmhSmweH03rl0EykgE4BNfDciO')
  ON CONFLICT ("user_name") DO NOTHING;
