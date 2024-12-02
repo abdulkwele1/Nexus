3780843073edf3961cc74febbac06cbb9078d363
Levi Schoen <l.schoen@kontakt.io>
2024-12-01 20:52:55 -0500

    add database types for solal panel kilowatts energy produced

--- nexus-api/clients/database/schemas/postgres/migrations/20241201202559_solar_panel_kilowatts_energy_produced.up.sql
+++ nexus-api/clients/database/schemas/postgres/migrations/20241201202559_solar_panel_kilowatts_energy_produced.up.sql
@@ -2,5 +2,6 @@
 CREATE TABLE solar_panel_kilowatts_energy_produced_data (
     id SERIAL PRIMARY KEY,          -- Auto-incrementing primary key
     date TIMESTAMP NOT NULL,        -- Date for the energy production data
-    production INTEGER NOT NULL     -- Amount of energy produced in kilowatts
+    production INTEGER NOT NULL,     -- Amount of energy produced in kilowatts
+    panel_id TEXT
 );
