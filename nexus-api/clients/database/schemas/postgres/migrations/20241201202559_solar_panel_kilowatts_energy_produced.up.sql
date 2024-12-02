d00d85f0b41257bba8bdf7e03d0100f34408e25f
Levi Schoen <l.schoen@kontakt.io>
2024-12-01 20:29:42 -0500

    add migration for solar panel kilowatts energy produced data

---
+++ nexus-api/clients/database/schemas/postgres/migrations/20241201202559_solar_panel_kilowatts_energy_produced.up.sql
@@ -0,0 +1,6 @@
+-- Create the table
+CREATE TABLE solar_panel_kilowatts_energy_produced_data (
+    id SERIAL PRIMARY KEY,          -- Auto-incrementing primary key
+    date TIMESTAMP NOT NULL,        -- Date for the energy production data
+    production INTEGER NOT NULL     -- Amount of energy produced in kilowatts
+);
