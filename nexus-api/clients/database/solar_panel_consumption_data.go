5305e5f5585e212d1e641095258db02e83fd140e
abdulkwele1 <168374411+abdulkwele1@users.noreply.github.com>
2025-01-10 13:06:35 -0800

    added types for ConsumptionData, defined methods to save the consumptionData to the table, defined a method to get the ConsumptionData from the panel

---
+++ nexus-api/clients/database/solar_panel_consumption_data.go
@@ -0,0 +1,43 @@
+package database
+
+import (
+	"context"
+	"errors"
+
+	"github.com/uptrace/bun"
+)
+
+var (
+	ErrorNoSolarPanelConsumptionData = errors.New("no energy production data found for panel")
+)
+
+type SolarPanelConsumptionData struct {
+	bun.BaseModel `bun:"table:solar_panel_consumption_data"`
+	ID            int     `bun:"id,pk,autoincrement"`
+	ConsumedKwh   float64 `bun:"consumed_kWh,notnull"`
+	CapacityKwh   float64 `bun:"capacity_kWh,notnull"`
+	PanelID       int     `bun:"panel_id,notnull"`
+}
+
+// Save saves the current consumption data to
+// the database, returning error (if any)
+func (spcd *SolarPanelConsumptionData) Save(ctx context.Context, db *bun.DB) error {
+	_, err := db.NewInsert().Model(spcd).Exec(ctx)
+
+	return err
+}
+
+func GetConsumptionDataForPanelID(ctx context.Context, db *bun.DB, panelID int) ([]SolarPanelConsumptionData, error) {
+	var solarPanelData []SolarPanelConsumptionData
+	err := db.NewSelect().Model(&solarPanelData).Where("panel_id = ?", panelID).Scan(ctx)
+
+	if err != nil {
+		return solarPanelData, err
+	}
+
+	if len(solarPanelData) == 0 {
+		return solarPanelData, ErrorNoSolarPanelConsumptionData
+	}
+
+	return solarPanelData, nil
+}
