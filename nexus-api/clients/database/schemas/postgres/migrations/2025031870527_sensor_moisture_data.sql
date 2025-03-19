d094ce1e97d85659593a45b90abc430d9f5a6d72
abdulkwele1 <168374411+abdulkwele1@users.noreply.github.com>
2025-03-18 19:17:46 -0700

    fixed naming inconsistency in the sensor moisture table

--- nexus-api/clients/database/schemas/postgres/migrations/2025031870527_sensor_moisture_data.sql
+++ nexus-api/clients/database/schemas/postgres/migrations/2025031870527_sensor_moisture_data.sql
@@ -2,6 +2,6 @@ CREATE TABLE sensor_moisture_data (
     sensor_id INTEGER NOT NULL,
     id SERIAL PRIMARY KEY,
     date TIMESTAMP WITH TIME ZONE NOT NULL,
-    moisture DECIMAL NOT NULL,
+    soil_moisture DECIMAL NOT NULL,
     FOREIGN KEY (sensor_id) REFERENCES sensors(id)
 );
