18e1b7d248b8c5950fa8618747c03ddc65091071
abdulkwele1 <168374411+abdulkwele1@users.noreply.github.com>
2025-03-18 19:17:08 -0700

    fixed naming incosistency for the sensor temp table

--- nexus-api/clients/database/schemas/postgres/migrations/2025031871023_sensor-temperature_data.sql
+++ nexus-api/clients/database/schemas/postgres/migrations/2025031871023_sensor-temperature_data.sql
@@ -2,6 +2,6 @@ CREATE TABLE sensor_temperature_data (
     id SERIAL PRIMARY KEY,
     sensor_id INTEGER NOT NULL,
     date TIMESTAMP WITH TIME ZONE NOT NULL,
-    temperature DECIMAL NOT NULL,
+    soil_temperature DECIMAL NOT NULL,
     FOREIGN KEY (sensor_id) REFERENCES sensors(id)
 );
