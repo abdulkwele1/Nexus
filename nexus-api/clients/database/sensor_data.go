57eb8cf90c4041b44369ef9b06f21f324ee72daa
abdulkwele1 <168374411+abdulkwele1@users.noreply.github.com>
2025-06-25 15:24:34 -0700

    Add sensor data structures and error handling
    - Introduced SensorMoistureData, SensorTemperatureData, SensorCoordinates, and Sensor types to represent sensor data and metadata.
    - Added error variables for handling cases where sensor moisture or temperature data is not found.
    - Enhanced the database package to support new sensor data structures for improved data management and retrieval.

--- nexus-api/clients/database/sensor_data.go
+++ nexus-api/clients/database/sensor_data.go
@@ -2,11 +2,44 @@ package database
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
 	"github.com/uptrace/bun"
 )
+
+var (
+	ErrorNoSensorMoistureData    = errors.New("no sensor moisture data found")
+	ErrorNoSensorTemperatureData = errors.New("no sensor temperature data found")
+)
+
+type SensorMoistureData struct {
+	ID           int       `bun:"id,pk,autoincrement"`
+	SensorID     int       `bun:"sensor_id"`
+	Date         time.Time `bun:"date,notnull"`
+	SoilMoisture float64   `bun:"soil_moisture"`
+}
+
+type SensorTemperatureData struct {
+	ID              int       `bun:"id,pk,autoincrement"`
+	SensorID        int       `bun:"sensor_id"`
+	Date            time.Time `bun:"date"`
+	SoilTemperature float64   `bun:"soil_temperature"`
+}
+
+type SensorCoordinates struct {
+	Latitude  float64 `bun:"latitude"`
+	Longitude float64 `bun:"longitude"`
+}
+
+type Sensor struct {
+	ID                int       `json:"id"`
+	Name              string    `json:"name"`
+	Location          string    `json:"location"`
+	InstallationDate  time.Time `json:"installation_date"`
+	SensorCoordinates `json:"sensor_coordinates"`
+}
 
 func GetSensorMoistureDataForSensorID(ctx context.Context, db *bun.DB, sensorID int) ([]SensorMoistureData, error) {
 	var data []SensorMoistureData
